97fa00bc3cc05c9041412d69c829768700b9dab1
Natewilliams91 <149424095+Natewilliams91@users.noreply.github.com>
2023-11-03 15:23:34 -0300

    Add files via upload

--- MoviesView.vue
+++ MoviesView.vue
@@ -52,11 +52,16 @@ export default {
       tasks: {},
       selectedDay: null,
       newTask: '',
-      calendar: [], // Added the missing calendar data property
+      calendar: [],
     };
   },
   mounted() {
     this.generateCalendar(this.currentYear, this.currentDate.getMonth());
+
+    // Retrieve tasks from localStorage on mount
+    if (localStorage.getItem('tasks')) {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
   },
   computed: {
     currentMonth() {
@@ -78,20 +83,16 @@ export default {
   this.currentDate = new Date(this.currentDate.setMonth(this.currentDate.getMonth() - 1));
   this.generateCalendar(this.currentYear, this.currentDate.getMonth());
 },
-
 nextMonth() {
   this.currentDate = new Date(this.currentDate.setMonth(this.currentDate.getMonth() + 1));
   this.generateCalendar(this.currentYear, this.currentDate.getMonth());
 },
-
     generateCalendar(year, month) {
       const daysInMonth = new Date(year, month + 1, 0).getDate();
       const calendar = [];
       let dayCounter = 1;
-
       for (let week = 0; week < 6; week++) {
         const weekDays = [];
-
         for (let day = 0; day < 7; day++) {
           if ((week === 0 && day < this.startingDay) || dayCounter > daysInMonth) {
             weekDays.push(''); // Add empty strings for days outside of the current month
@@ -100,10 +101,8 @@ nextMonth() {
             dayCounter++;
           }
         }
-
         calendar.push(weekDays);
       }
-
       this.calendar = calendar;
     },
     onMouseOver() {
@@ -117,23 +116,33 @@ nextMonth() {
       // When a day is selected, compute the full date string and store it
       this.selectedFullDate = `${this.formattedCurrentDate}-${String(day).padStart(2, '0')}`;
     },
+
     addTask() {
       if (!this.tasks[this.selectedFullDate]) {
-        this.tasks[this.selectedFullDate] = []; // Initialize an array for the day if it doesn't exist
+        this.tasks[this.selectedFullDate] = [];
       }
       if (this.newTask.trim() !== '') {
-        this.tasks[this.selectedFullDate].push(this.newTask); // Add the new task to the selected day
-        this.newTask = ''; // Clear the input field
+        this.tasks[this.selectedFullDate].push(this.newTask);
+        this.newTask = '';
       }
+
+      // Save tasks to localStorage after adding a task
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
     },
+
     removeTask(day, index) {
-      if (this.tasks[this.selectedFullDate] && this.tasks[this.selectedFullDate].length > index) {
-        this.tasks[this.selectedFullDate].splice(index, 1); // Remove the task at the specified index
+      const dayString = `${this.formattedCurrentDate}-${String(day).padStart(2, '0')}`;
+      if (this.tasks[dayString] && this.tasks[dayString].length > index) {
+        this.tasks[dayString].splice(index, 1);
       }
+
+      // Save tasks to localStorage after removing a task
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
     },
   },
 };
 </script>
+
 
 <style>
 .navigation {
@@ -142,18 +151,15 @@ nextMonth() {
   align-items: center;
   padding: 10px;
 }
-
 h1 {
   margin: 0;
   font-size: 2rem;
   color: rgb(28, 187, 240);
 }
-
 .subtitle {
   margin: 10px 0;
   font-size: 1.5rem;
 }
-
 button {
   background-color: rgb(28, 187, 240);
   color: #ffffff;
@@ -163,7 +169,6 @@ button {
   border-radius: 5px;
   transition: background-color 0.3s ease;
 }
-
 button:hover {
   background-color: rgb(14, 143, 192);
 }
@@ -174,31 +179,26 @@ button:hover {
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
     text-align: center;
   }
-
   .title {
     color: rgb(28, 187, 240);
     font-size: 2.5rem;
     margin-top: 0;
     margin-bottom: 30px;
   }
-
   .table-container {
     display: flex;
     justify-content: center;
   }
-
   .table {
     width: 80%;
     border-collapse: collapse;
   }
-
   .day-header {
     background-color: rgb(28, 187, 240);
     color: #ffffff; /* Changed text color to white for better visibility */
     text-align: center;
     padding: 10px;
   }
-
   .calendar-day {
     width: 80px;
     height: 80px;
@@ -208,35 +208,29 @@ button:hover {
     cursor: pointer;
     position: relative;
   }
-
   .calendar-day:hover {
     background-color: rgba(28, 187, 240, 0.1); /* Lightened blue tone for hover effect */
   }
-
   .day-number {
     font-size: 1.2rem;
     color: rgb(28, 187, 240); /* Same blue tone for the day numbers */
   }
-
   .task-input-container {
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 10px;
   }
-
   .task-input {
     width: 120px;
     padding: 5px;
     border: 1px solid rgb(28, 187, 240); /* Blue border for the input field */
   }
-
   .task-list {
     list-style-type: none;
     padding: 0;
     margin: 0;
   }
-
   .task-list li button {
     background-color: rgb(28, 187, 240);
     color: #ffffff;
@@ -246,11 +240,9 @@ button:hover {
     border-radius: 5px;
     transition: background-color 0.3s ease; /* Added transition for smooth color change */
   }
-
   .task-list li button:hover {
     background-color: rgb(14, 143, 192); /* Darkened blue tone for hover effect */
   }
-
   .task-item {
     display: flex;
     justify-content: space-between;
@@ -262,27 +254,21 @@ button:hover {
     overflow: hidden;
     text-overflow: ellipsis;
   }
-
 .month-navigation {
   display: flex;
   align-items: center;
   gap: 10px; /* Separation between elements */
 }
-
 .subtitle {
   margin: 0;
 }
-
 .navigation {
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 10px;
 }
-
 h1 {
   margin-bottom: 10px; /* Give a space between the title and the navigation */
 }
-
 </style>
-
